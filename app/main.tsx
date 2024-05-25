13a9f7a9a9ab9ec2d044ba53f154f7ea3da73e95
Felipe Stamboni <felipestambonii@gmail.com>
2024-05-24 21:45:24 -0300

    Adiconei o template de filtros e adicionei ele na página principal

--- app/main.tsx
+++ app/main.tsx
@@ -1,12 +1,15 @@
-import { Link } from "expo-router";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 import MenuPrincipal from "../src/components/menuprincipal/MenuPrincipal";
 import Menu from "../src/components/menu/Menu";
+import Filtros from "../src/components/filtros/Filtros";
 
 export default function Main() {
   return (
-    <View className="flex-1">
+    <View>
       <MenuPrincipal />
+      <View className="h-[500px]">
+      <Filtros />
+      </View>
       <Menu />
     </View>
   );
